93209c17fa46f102cce941bac6bb7345c19551a4
Tkwok7 <tkwok7@gatech.edu>
2024-03-08 10:44:24 -0500

    small angle fix

--- Assets/Scripts/CaptioningScene/Captioning/CaptionBackground.cs
+++ Assets/Scripts/CaptioningScene/Captioning/CaptionBackground.cs
@@ -84,7 +84,7 @@ public class CaptionBackground : MonoBehaviour
         //  (float)Math.Sqrt((dist * dist - Math.Pow(backgroundRect.transform.position.x, 2) - Math.Pow(backgroundRect.transform.position.y, 2))));
             
         //  make the Container look at the camera
-        transform.rotation = Quaternion.LookRotation(forwardFromCamera);
+        transform.rotation = Quaternion.LookRotation(rotatedForward);
     }
 
     void HandleRegCaptions() {
