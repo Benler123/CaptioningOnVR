29d31d8f7d68322d6b5847043dc75f9afe9a3d35
Devin Zhang <devinzhang415@gmail.com>
2024-03-12 19:24:47 -0400

    auto captioning added

--- Tools/auto_gen_captions.py
+++ Tools/auto_gen_captions.py
@@ -12,3 +12,26 @@
 
 # here is a package that wraps all of whisper and whisper-timestamped, might be easier to use
 # https://github.com/openai/whisper/discussions/1093
+
+import whisper_timestamped as whisper
+import json
+import pathlib
+
+def main():
+    video_name = "main.1.mp4"
+
+    video_path = str(pathlib.Path(__file__).parent.parent.absolute()) + "/Assets/StreamingAssets/Videos/" + video_name
+    audio = whisper.load_audio(video_path)
+
+    model = whisper.load_model("tiny", device="cpu")
+    result = whisper.transcribe(model, audio, language="en")
+
+    json_name = video_name + ".json"
+    json_path = str(pathlib.Path(__file__).parent.absolute()) + "/" + json_name
+    print(json_name)
+    print(json_path)
+    with open(json_path, "w+") as f:
+        json.dump(result, f)
+
+if __name__ == "__main__":
+    main()
